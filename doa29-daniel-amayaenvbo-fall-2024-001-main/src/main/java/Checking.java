bdb8c95c49a1d0633fbb9b0a3c9d08279bed724b
Daniel Amayaenvbo <159960133+doa29@users.noreply.github.com>
2024-11-10 23:30:15 -0500

    Updated Checking.java

--- doa29-daniel-amayaenvbo-fall-2024-001-main/src/main/java/Checking.java
+++ doa29-daniel-amayaenvbo-fall-2024-001-main/src/main/java/Checking.java
@@ -1,5 +1,11 @@
+// Daniel Amayaenvbo, doa29
+// Last Update: November 10th 2024
+
+// Class representing a Checking account, extending the Account class
 public class Checking extends Account {
+
+	// Constructor to initialize a Checking account with an ID and APR, setting initial balance to 0
 	public Checking(String id, double apr) {
-		super(id, apr, 0);
+		super(id, apr, 0); // Call the constructor of the Account class with an initial balance of 0
 	}
 }
