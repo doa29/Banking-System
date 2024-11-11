d33755d4a43ac80d39dcac60a2c9711ff7e63c6b
Daniel Amayaenvbo <159960133+doa29@users.noreply.github.com>
2024-11-10 23:29:00 -0500

    Updated CD.java

--- doa29-daniel-amayaenvbo-fall-2024-001-main/src/main/java/CD.java
+++ doa29-daniel-amayaenvbo-fall-2024-001-main/src/main/java/CD.java
@@ -1,5 +1,11 @@
+// Daniel Amayaenvbo,doa29
+// Last Update: November 10, 2024
+
+// Class representing a Certificate of Deposit (CD) account, extending the Account class
 public class CD extends Account {
+
+	// Constructor to initialize a CD account with an ID, APR, and initial balance
 	public CD(String id, double apr, double initialBalance) {
-		super(id, apr, initialBalance);
+		super(id, apr, initialBalance); // Call the constructor of the Account class to initialize common properties
 	}
 }
